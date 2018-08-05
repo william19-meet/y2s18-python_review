e391788127ac9bbb7a09fc37ff08e96aef3b2623
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 11:59:51 +0300

    PYTHON

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,5 @@
-# Write your solution for 1.2 here!
+a = 0
+for i in range(101):
+    if i % 2 == 0:
+        a = a + i
+    print(a)
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,4 @@
-# Write your solution for 1.1 here!
+a = 0
+for i in range(101):
+    a = a + i
+    print(a)
